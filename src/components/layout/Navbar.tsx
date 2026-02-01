357f624102398d60c46ef0ba3e2b49566739d67f
Kholod13 <vlad13holod@gmail.com>
2026-02-01 19:08:56 +0100

    finish Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
 import { LayoutDashboard, Wallet, History, Settings } from 'lucide-react';
 import logo from '../../assets/logo.svg';
+import codeBy from '../../assets/codeBy.png';
 
+// Pages list
 const NAV_ITEMS = [
     {id: 'dashboard', label: 'Dashboard', path: '/dashboard', icon: LayoutDashboard},
     {id: 'wallets', label: 'Wallets', path: '/wallets', icon: Wallet},
@@ -11,33 +13,41 @@ const NAV_ITEMS = [
 
 export const Navbar = () => {
     return (
-        <nav>
-            <div className="flex items-center gap-3 px-2 mb-10">
-                <img src={logo} alt="Logo" className="w-8 h-8" />
-                <span className="text-white font-bold text-xl tracking-tight">CryptoPulse</span>
+        <nav className="flex flex-col p-4 h-screen">
+            {/* Flex block for (h-screen/flex-1) */}
+            <div className="flex flex-col flex-1">
+                {/* Logotype */}
+                <div className="flex items-center gap-3 px-2 ml-2 mr-2 mt-2 mb-5">
+                    <img src={logo} alt="Logo" className="w-14 h-14" />
+                    <span className="font-bold text-2xl">CryptoWallet</span>
+                </div>
+                {/* List of pages array */}
+                <ul className=" flex flex-col gap-3 flex-auto">
+                    {NAV_ITEMS.map((item) => (
+                        <li key={item.id}>
+                            <NavLink
+                                to={item.path}
+                                className={({ isActive }) => `
+                                    flex font-bold items-center gap-3 px-5 py-6 rounded-lg transition-all duration-800
+                                    ${isActive ? 'bg-[#362F5E] text-white shadow-lg shadow-blue-900/20'
+                                    : 'text-slate-400 hover:bg-slate-800 hover:text-slate-200 hover:transition-all duration-600'}
+                                `}
+                            >
+                                <item.icon size={24} />
+                                <span>{item.label}</span>
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+            {/* Footer of navigation / text: no copyrighted */}
+            <div className="flex flex-col items-center gap-8 px-2 ml-2 mr-2 mt-2 mb-5 bg-gray-400
+            p-6 rounded-xl">
+                <img src={codeBy} alt="code_icon" className="w-40 h-40" />
+                <p className="font-bold text-wrap text-sm text-gray-600 text-center">© 2026 Code by Vladyslav K.</p>
             </div>
 
-            <ul>
-                {NAV_ITEMS.map((item) => (
-                    <li key={item.id}>
-                        <NavLink
-                            to={item.path}
-                            className={({ isActive }) => `
-                                flex items-center gap-3 px-4 py-3 rounded-xl transition-all duration-200
-                                ${isActive ? 'bg-blue-600 text-white shadow-lg shadow-blue-900/20'
-                                : 'text-slate-400 hover:bg-slate-800 hover:text-slate-200'}
-                            `}
-                        >
-                            <item.icon size={20} />
-                            <span>{item.label}</span>
-                        </NavLink>
-                    </li>
-                ))}
-            </ul>
-
-            <div>
-                <p>fofo</p>
-            </div>
         </nav>
     )
 }
